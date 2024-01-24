16d402edc341c7ef38c2319884f88f55a5305123
athenanguyen <athenanguyen@athenas-mbp-2.dhcp.wustl.edu>
2024-01-24 14:02:01 -0600

    studio 1 complete

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,7 +15,14 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
-
+		boolean test1 = x < y;
+		boolean test2 = y < z;
+		boolean test3 = test1 && test2;
+		boolean test4 = x > y;
+		boolean test5 = y > z;
+		boolean test6 = test4 && test5;
+		boolean isOrdered = test3 || test6;		
+System.out.println(isOrdered);
 	}
 
 }
